e53d7ad3da3d4b54feacd8b9a7ed282d6fd9a282
Jay Koontz <jkoontz2010@gmail.com>
2017-06-07 17:42:51 -0700

    added CSS to LoginForm

--- scripts/build-dist.js
+++ scripts/build-dist.js
@@ -4,11 +4,14 @@ var fs = require('fs');
 sass.render({
   file: './lib/styles/main.scss'
 }, function(error, result) { // node-style callback from v3.0.0 onwards
+  
   if(!error){
     fs.writeFile('./build/main.css', result.css, function(err){
       if(!err){
         console.log('css file written to disk')
       }
     });
+  } else {
+    console.error(error)
   }
 });
