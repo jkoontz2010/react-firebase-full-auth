cb9f6ad4ebb06bddd6afb5622a3a462a2565ec3b
Jay Koontz <jkoontz2010@gmail.com>
2017-06-22 15:50:40 -0700

    unit tests for signup form

--- __tests__/signup-form.spec.js
+++ __tests__/signup-form.spec.js
@@ -1,13 +1,31 @@
 import SignupForm from '../lib/components/SignupForm';
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 
 describe('SignupForm', () => {
-    it('has a field for email', () => {
+    let wrapper;
 
+    beforeEach(() => {
+        wrapper = mount(<SignupForm />);
+    })
+    it('should render', () => {
+        expect(wrapper.length).toEqual(1);
+    })
+
+    it('has a field for email', () => {
+        expect(wrapper.find('[name="email"]').length).toEqual(1);
     });
 
     it('has a field for password', () => {
+        expect(wrapper.find('[name="password"]').length).toEqual(1);
+    })
+
+    it('does not allow submission when processing prior submission', () => {
+        const submitButton = wrapper.find('button [type="submit"]');
+  
+        submitButton.simulate('click');
+        expect(submitButton.props().disabled).toEqual(true);
 
     })
+
 });
